9e15b3f44dd081f130df7617f0b835875322ef5b
nhuhung19 <nhuhung19@gmail.com>
2020-04-21 23:53:25 +0700

    note in bookControll

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -5,13 +5,13 @@ exports.createBook = async (req, res) => {
     const { title, genres, author, description } = req.body
     console.log(title, genres, author, description)
     
-    const book = new Book({
+    const book = new Book({ // create new instance iherence class Book
         title: title,
         description: description, 
         genres: genres,
         author: author
     })
-    await book.save()
+    await book.save() // chang title, des, genres, author and save in database
     res.status(201).json({status:"ok", data: book})
 
 }
